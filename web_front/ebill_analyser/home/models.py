3af9193077fd89b175c31542b2083252f0e80b77
Kasun Thennakoon <tmkasun@gmail.com>
2016-09-19 23:17:01 +0530

    new analyse parameter

--- web_front/ebill_analyser/home/models.py
+++ web_front/ebill_analyser/home/models.py
@@ -17,9 +17,21 @@ class Ebill(Document):
     def get_call_frequencies():
         frequencies = Ebill.objects.aggregate({'$group': {'_id': "$number", 'count': {'$sum': 1}}},
                                               {'$sort': {"count": -1}})
+        return Ebill._cursor_to_list(frequencies)
+
+    @staticmethod
+    def get_calls_per_day():
+        calls_per_day = Ebill.objects.aggregate(
+                {'$project': {'ymd': {'$dateToString': {'format': "%Y-%m-%d", 'date': "$timestamp"}}}},
+                {'$group': {'_id': "$ymd", 'count': {'$sum': 1}}},
+                {'$sort': {"_id": -1}})
+        return Ebill._cursor_to_list(calls_per_day)
+
+    @staticmethod
+    def _cursor_to_list(cursor):
         details = []
-        current = next(frequencies, None)
+        current = next(cursor, None)
         while current:
             details.append(current)
-            current = next(frequencies, None)
+            current = next(cursor, None)
         return details
