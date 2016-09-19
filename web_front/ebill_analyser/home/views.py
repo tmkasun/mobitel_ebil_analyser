4a8cb7a749df77e70b14e594b37251bba9012e64
Kasun Thennakoon <tmkasun@gmail.com>
2016-09-19 23:17:35 +0530

    add new UI for calls per day frequency viz

--- web_front/ebill_analyser/home/views.py
+++ web_front/ebill_analyser/home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from home.models import Ebill
 
+
 def index(request):
     frequencies = Ebill.get_call_frequencies()
-    cont = {'frequencies': frequencies}
+    calls = Ebill.get_calls_per_day()
+    cont = {'frequencies': frequencies, 'calls': calls}
     return render(request, 'home/index.html', cont)
