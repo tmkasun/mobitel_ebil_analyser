29efde65f4ff6c73116cc1b3e9e292861bb26da2
Kasun Thennakoon <tmkasun@gmail.com>
2016-09-20 10:31:00 +0530

    Move inline JS to seperate init script

--- web_front/ebill_analyser/home/static/js/init.js
+++ web_front/ebill_analyser/home/static/js/init.js
@@ -1,7 +1,28 @@
-(function($){
-  $(function(){
+var fullLineData;
+(function ($) {
+    $(function () {
 
-    $('.button-collapse').sideNav();
-
-  }); // end of document ready
+        $('.button-collapse').sideNav();
+        $('.modal-trigger').leanModal({
+            ready: function () {
+                drawCallDistro('call-distro-full');
+            }
+        });
+        drawCallDistro('call-distro-mini');
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function drawCallDistro(element) {
+    var element_id = '#'+element;
+    $(element_id).empty();
+    Morris.Line({
+        element: element,
+        data: fullLineData,
+        xkey: 'y',
+        ykeys: ['a'],
+        xLabels: "day",
+        axes: true,
+        resize: true,
+        labels: ['# Calls']
+    });
+}
